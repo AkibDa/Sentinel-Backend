cd79da295d7d42968450eba5682c21179b308cf2
StackUnderflow10 <imsarkarjeet012006@gmail.com>
2026-04-16 20:10:01 +0530

    updated the downloader code

--- app/services/downloader.py
+++ app/services/downloader.py
@@ -5,13 +5,13 @@ def download_file(url):
     filename = f"temp_{uuid.uuid4()}.mp4"
 
     headers = {
-        "User-"
+        "User-Agent": "Mozilla/5.0"
     }
 
-    response = requests.get(url, stream=True)
+    response = requests.get(url, headers=headers,stream=True)
 
     if response.status_code != 200:
-        raise Exception("Download Failed")
+        raise Exception(f"Download failed with status {response.status_code}")
     
     with open(filename, "wb") as f:
         for chunk in response.iter_content(1024):
